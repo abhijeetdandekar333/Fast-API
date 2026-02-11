17a9aaae5e9aa95030f9d3ac67a97b222f5d8b9c
Abhijeet Dandekar <abhijeetdandekar333@gmail.com>
2026-02-12 01:03:34 +0530

    added .env

--- app/config/db.py
+++ app/config/db.py
@@ -1,10 +1,22 @@
 from pymongo import MongoClient
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+import os
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+
+load_dotenv()  # loads .env file
+
 
 app = FastAPI()
 
 
-conn = MongoClient("mongodb+srv://abhijeet_dandekar_db:abcd%401234@cluster0.npaoyco.mongodb.net")
 
+username = os.getenv("MONGO_USERNAME")
+password = quote_plus(os.getenv("MONGO_PASSWORD", ""))
+host = os.getenv("MONGO_HOST")
+db_name = os.getenv("MONGO_DB")
 
+MONGO_URI = f"mongodb+srv://{username}:{password}@{host}/{db_name}"
+
+conn = MongoClient(MONGO_URI)
